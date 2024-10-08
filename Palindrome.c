cf47c89f10f6022c967f9f20252924f30b6206db
Madhusri182007 <madhusri182007@gmail.com>
2024-10-08 19:54:46 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int N,rem,Sum=0,t;
+    printf("Enter tha number:");
+    scanf("%d",&N);
+    t=N;
+    while(N>0)
+    {
+        rem=N%10;
+        Sum=Sum*10+rem;
+        N=N/10;
+    }
+    if(t==Sum)
+    {
+        printf("Palindrome Number");
+    }
+    else
+    {
+        printf("Not a Palindrome Number");
+    }
+}
