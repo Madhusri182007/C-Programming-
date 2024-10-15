484d79aa2e049dcd5ee7f95bcf3ac748c1b670ce
Madhusri182007 <madhusri182007@gmail.com>
2024-10-15 20:23:35 +0530

    Update Arrays_Percentage_Sum_Average.c

--- Arrays_Percentage_Sum_Average.c
+++ Arrays_Percentage_Sum_Average.c
@@ -7,12 +7,24 @@ int main()
     int Percentage[N];
     for(i=0;i<N;i++)
     {
+        printf("Enter Person %d:",i+1);
         scanf("%d",&Percentage[i]);
     }
     for(i=0;i<N;i++)
     {
         printf("Person %d:%d\n",i+1,Percentage[i]);
         Sum=Sum+Percentage[i];
+    }
+    for(i=0;i<N;i++)
+    {
+        if(Percentage[i]>=50)
+        {
+            printf("Person %d:%d(Passed)\n",i+1,Percentage[i]);
+        }
+        else if(Percentage[i]<50)
+        {
+            printf("Person %d:%d(Failed)\n",i+1,Percentage[i]);
+        }
     }
     Average=((float)Sum)/N-1;
     printf("Class Total:%d\n",Sum);
