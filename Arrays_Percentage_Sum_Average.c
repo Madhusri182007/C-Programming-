0dfb3c793ccb7b7f305194d590d73728396214fa
Madhusri182007 <madhusri182007@gmail.com>
2024-10-15 20:05:42 +0530

    Create Arrays_Percentage_Sum_Average.c

---
+++ Arrays_Percentage_Sum_Average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int N,Sum=0,i; 
+    float Average;
+    scanf("%d",&N);
+    int Percentage[N];
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&Percentage[i]);
+    }
+    for(i=0;i<N;i++)
+    {
+        printf("Person %d:%d\n",i+1,Percentage[i]);
+        Sum=Sum+Percentage[i];
+    }
+    Average=((float)Sum)/N-1;
+    printf("Class Total:%d\n",Sum);
+    printf("Class Average:%.2f\n",Average);
+    return 0;
+}
