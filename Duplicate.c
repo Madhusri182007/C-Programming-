fab21179fdb40a07177c0c15487975d8551d1ee1
Madhusri182007 <madhusri182007@gmail.com>
2024-10-20 18:19:07 +0530

    Update Duplicate.c

--- Duplicate.c
+++ Duplicate.c
@@ -5,29 +5,22 @@ int main()
   printf("Enter array size:");
   scanf("%d",&N);
   int Num[N];
-  int Num1[N],x=0;
   for(i=0;i<N;i++)
   {
       scanf("%d",&Num[i]);
   }
+  count=0;
   for(i=0;i<=N-1;i++)
   {
-      count=0;
       for(j=i+1;j<N;j++)
       {
           if(Num[i]==Num[j])
           {
               count++;
-              if(count==1)
-              {
-                 Num1[x++] =Num[i];
-              }
+              
+            break;
           }
       }
   }
-  printf("Repeated Numbers:");
-  for(i=0;i<x;i++)
-  {
-      printf("%d",Num1[i]);
-  }
+  printf("count:%d",count);
 }
