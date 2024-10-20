60346d473bdd83b51639feb8e78af302dc8fb13f
Madhusri182007 <madhusri182007@gmail.com>
2024-10-20 11:55:59 +0530

    Create Pattern_row.c

---
+++ Pattern_row.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int row,col;
+    for(row=1;row<=N;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
