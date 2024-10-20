ee0dd5ee48f5d5f3777f3b4e14c148d643d261e3
Madhusri182007 <madhusri182007@gmail.com>
2024-10-20 17:24:52 +0530

    Update Patten_Consecutive_No.c

--- patten_Consecutive_No.c
+++ patten_Consecutive_No.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int N,t=1;
+    int N,row,col,t=1;
     scanf("%d",&N);
-    int row,col;
-    for(row=1;row<=N;row++)
+    for(row=0;row<N;row++)
     {
-        for(col=1;col<=row;col++)
+        for(col=0;col<=row;col++)
         {
             printf("%d ",t);
             t++;
         }
         printf("\n");
     }
-    return 0;
 }
