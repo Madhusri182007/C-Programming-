22b48e84aac85e398b6cf4dc617faf798861e766
Madhusri182007 <madhusri182007@gmail.com>
2024-10-21 20:59:30 +0530

    Update and rename Fibonacci.c to Fibonacci_reve_fibonacii.c

--- Fibonacci_reve_fibonacii.c
+++ Fibonacci_reve_fibonacii.c
@@ -8,12 +8,20 @@ int main()
     int Num1=0;
     int Num2=1;
     int arr[N];
+    arr[0]=Num1;
+    arr[1]=Num2;
     printf("%d %d",Num1,Num2);
-    for(int i=1;i<=N-2;i++)
+    for(int i=2;i<N;i++)
     {
         res=Num1+Num2;
+        arr[i]=res;
         Num1=Num2;
         Num2=res;
         printf("%d ",res);
+    }
+    printf("\n");
+    for(int j=N-1;j>=0;j--)
+    {
+        printf("%d ",arr[j]);
     }
 }
