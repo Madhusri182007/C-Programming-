8ac55f2a142511e2e19bbd4fc404582532b46035
Madhusri182007 <madhusri182007@gmail.com>
2024-10-22 18:40:13 +0530

    Create Matrix_Subtraction.c

---
+++ Matrix_Subtraction.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int R1,C1,R2,C2,i,j;
+    printf("Enter number of rows:");
+    scanf("%d %d %d %d",&R1,&C1,&R2,&C2);
+    int a[R1][C1],b[R2][C2];
+    if(R1==R2 && C1==C2)
+    {
+       printf("Enter Matrix1:\n");
+        for( i=0;i<R1;i++)
+       {
+        for( j=0;j<C1;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+       }
+          printf("Enter Matrix2:\n");
+        for(i=0;i<R2;i++)
+      {
+        for(j=0;j<C2;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+      }
+      int Sub[R1][C2];
+      for(i=0;i<R1;i++)
+      {
+          for(j=0;j<C1;j++)
+          {
+              Sub[i][j]=a[i][j]-b[i][j];
+          }
+      }
+      printf("Addition matrix:\n");
+      for(i=0;i<R1;i++)
+      {
+          for(j=0;j<C1;j++)
+          {
+              printf(" %d ",Sub[i][j]);
+          }
+          printf("\n");
+      }
+    }
+    else
+    {
+        printf("Given row1 and row2 is not equal  or Given col1 and col2 is not equal ");
+    }
+    
+    return 0;
+}
