01b76755dbcba7d62cb7296f7ee623b7fa4489b7
Madhusri182007 <madhusri182007@gmail.com>
2024-11-05 19:55:09 +0530

    Create Array_left_rotate.c

---
+++ Array_left_rotate.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int N,i,j;
+    scanf("%d",&N);
+    int a[N];
+    printf("Enter array elemnts:");
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int r;
+    printf("Enter number of times to rotate:");
+    scanf("%d",&r);
+    int C[N];
+    int k=1;
+    while(k<=r)
+    {
+        for(j=0;j<N;j++)
+        {
+            if(j!=N-1)
+            {
+            C[j]=a[j+1];
+            }
+            else
+            {
+                C[j]=a[0];
+            }
+        }
+            for(i=0;i<N;i++)
+            {
+               a[i]=C[i];
+            }
+            k++;
+    }
+    printf("Roted array:");
+    for(i=0;i<N;i++)
+    {
+        printf("%d ",C[i]);
+    }
+}
